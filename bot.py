10de0e415bafefb7b9023b3853388cd96f404fc8
Sтσям <118429184+StormBeatz@users.noreply.github.com>
2023-09-07 07:10:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,14 +70,14 @@ async def download_image(url):
     return TMP_DOWNLOAD_DIRECTORY + "pinterest_iamge.jpg"
 
 
-APP_ID = os.environ.get("APP_ID", None)
-APP_HASH = os.environ.get("APP_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+APP_ID = os.environ.get("APP_ID", "23540755")
+APP_HASH = os.environ.get("APP_HASH", "5e44360867398fcb1e17d442ab255e51")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6680045631:AAHPWy6jM__yP6Qx_15884Gn6OBeBrPiHlQ")
 TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
-MONGO_DB = os.environ.get("MONGO_DB", None)
+MONGO_DB = os.environ.get("MONGO_DB", "mongodb+srv://Pint:Pint@cluster0.qkd60oh.mongodb.net/?retryWrites=true&w=majority")
 # type yout telegram id or username
-LOG = os.environ.get("LOG", None)
-ADMIN = os.environ.get("ADMIN", None)
+LOG = os.environ.get("LOG", "-1001940418717")
+ADMIN = os.environ.get("ADMIN", "6382479526")
 
 
 bot = TelegramClient("pinterestbot", APP_ID, APP_HASH).start(bot_token=BOT_TOKEN)
